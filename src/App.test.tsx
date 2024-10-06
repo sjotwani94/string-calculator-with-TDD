10405d7c6edd19d792252554ee7a506dfd88cf7c
sjotwani94 <siddharthkj345@gmail.com>
2024-10-06 18:09:07 +0530

    Create UI Design for the Application using Bootstrap with Basic Test Cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App.tsx Component', () => {
+    describe('render function', () => {
+        test('Renders Mentioned Title inside Header', () => {
+            render(<App />);
+            const linkElement = screen.getByText(/String Calculator Developed using TDD/i);
+            expect(linkElement).toBeInTheDocument();
+            expect(linkElement.tagName).toBe('HEADER');
+        });
+
+        test('Renders A Button to call Calculate Result Function', () => {
+            render(<App />);
+            const buttonElement = screen.getByText(/Calculate Result!/i);
+            expect(buttonElement).toBeInTheDocument();
+            expect(buttonElement.tagName).toBe('BUTTON');
+        });
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Card, Form } from 'react-bootstrap';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <header className="app-container-header">String Calculator Developed using TDD</header>
+            <div className="app-container-body">
+                <Card style={{ width: '30rem' }}>
+                    <Card.Body>
+                        <Card.Title>String Calculator</Card.Title>
+                        <Card.Text>
+                            <Form>
+                                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                    <Form.Label>Type A Numerical Expression Here:</Form.Label>
+                                    <Form.Control size="lg" type="text" placeholder="Example: '1,2,3,4'" />
+                                </Form.Group>
+                            </Form>
+                        </Card.Text>
+                        <Button variant="primary">Calculate Result!</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
+    );
 }
 
 export default App;
